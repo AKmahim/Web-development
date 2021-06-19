01af9e3555a45887ac3b30cd6e086bc2bdca1fbb
AKmahim <mdmahimhossen134@gmail.com>
2021-06-19 21:31:14 +0600

    19June2021

---
+++ Mobile-First-website/js/app.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+
+const navList = document.querySelector('.nav-list');
+
+
+hamburger.addEventListener('click',()=>{
+
+	navList.classList.toggle('show');
+
+});
+
+
+
